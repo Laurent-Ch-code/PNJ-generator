c6c1dad0d20117d42121bfd1c3ff2dfd5b996a94
laurent.chauveau@yahoo.fr <laurent.chauveau@yahoo.fr>
2026-01-25 10:19:46 +0100

    Modification routing depuis le menu

--- front-pnj-generator/src/app/app.component.ts
+++ front-pnj-generator/src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, signal } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +9,9 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
 
+  private readonly router = inject(Router);
+
+  navigateUniverses(): void {
+    this.router.navigate(['/universes']);
+  }
 }
