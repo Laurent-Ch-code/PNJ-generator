42845d82a7be6f08eb9c56c5b9d73fc94e680f07
laurent.chauveau@yahoo.fr <laurent.chauveau@yahoo.fr>
2026-01-29 10:54:47 +0100

    Gestion appel vers l'API, et envoi de l'ID de l'univers vers weapons-crud

--- front-pnj-generator/src/app/features/univers/universe-detail/universe-detail.component.ts
+++ front-pnj-generator/src/app/features/univers/universe-detail/universe-detail.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { WeaponsCrudComponent } from '../../weapons/weapons-crud/weapons-crud.component';
+
 
 @Component({
   selector: 'app-universe-detail',
-  imports: [],
+  imports: [WeaponsCrudComponent],
   templateUrl: './universe-detail.component.html',
   styleUrl: './universe-detail.component.scss'
 })
-export class UniverseDetailComponent {
+export class UniverseDetailComponent implements OnInit {
 
+  private readonly route = inject(ActivatedRoute);
+  private readonly router = inject(Router);
+  universeId: string | null = null;
+
+  ngOnInit(): void {
+    this.universeId = this.route.snapshot.paramMap.get('id');
+  }
 }
